45c0a29d8ff438a40602307d675c30fa3a7e35e9
VVTretyak <vladitretyak2@gmail.com>
2022-05-03 18:31:35 +0300

    AccessTest

---
+++ AccessTest/AccessTestV2/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace AccessTestV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.кодDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.имяDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.accessDbTestDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.accessDbTestDataSet = new AccessTestV2.AccessDbTestDataSet();
+            this.personBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personTableAdapter = new AccessTestV2.AccessDbTestDataSetTableAdapters.PersonTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ContentTb = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accessDbTestDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accessDbTestDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.кодDataGridViewTextBoxColumn,
+            this.имяDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.personBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(241, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // кодDataGridViewTextBoxColumn
+            // 
+            this.кодDataGridViewTextBoxColumn.DataPropertyName = "Код";
+            this.кодDataGridViewTextBoxColumn.HeaderText = "Код";
+            this.кодDataGridViewTextBoxColumn.Name = "кодDataGridViewTextBoxColumn";
+            // 
+            // имяDataGridViewTextBoxColumn
+            // 
+            this.имяDataGridViewTextBoxColumn.DataPropertyName = "Имя";
+            this.имяDataGridViewTextBoxColumn.HeaderText = "Имя";
+            this.имяDataGridViewTextBoxColumn.Name = "имяDataGridViewTextBoxColumn";
+            // 
+            // personBindingSource1
+            // 
+            this.personBindingSource1.DataMember = "Person";
+            this.personBindingSource1.DataSource = this.accessDbTestDataSetBindingSource;
+            // 
+            // accessDbTestDataSetBindingSource
+            // 
+            this.accessDbTestDataSetBindingSource.DataSource = this.accessDbTestDataSet;
+            this.accessDbTestDataSetBindingSource.Position = 0;
+            // 
+            // accessDbTestDataSet
+            // 
+            this.accessDbTestDataSet.DataSetName = "AccessDbTestDataSet";
+            this.accessDbTestDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // personBindingSource
+            // 
+            this.personBindingSource.DataMember = "Person";
+            this.personBindingSource.DataSource = this.accessDbTestDataSet;
+            // 
+            // personTableAdapter
+            // 
+            this.personTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Remove";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ContentTb
+            // 
+            this.ContentTb.Location = new System.Drawing.Point(269, 12);
+            this.ContentTb.Name = "ContentTb";
+            this.ContentTb.Size = new System.Drawing.Size(100, 20);
+            this.ContentTb.TabIndex = 2;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(281, 38);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.Location = new System.Drawing.Point(281, 67);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(75, 23);
+            this.UpdateBtn.TabIndex = 4;
+            this.UpdateBtn.Text = "Update";
+            this.UpdateBtn.UseVisualStyleBackColor = true;
+            this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 179);
+            this.Controls.Add(this.UpdateBtn);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.ContentTb);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Access";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accessDbTestDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accessDbTestDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private AccessDbTestDataSet accessDbTestDataSet;
+        private System.Windows.Forms.BindingSource personBindingSource;
+        private AccessDbTestDataSetTableAdapters.PersonTableAdapter personTableAdapter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ContentTb;
+        private System.Windows.Forms.DataGridViewTextBoxColumn кодDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn имяDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource personBindingSource1;
+        private System.Windows.Forms.BindingSource accessDbTestDataSetBindingSource;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button UpdateBtn;
+    }
+}
+
---
+++ AccessTest/AccessTestV2/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace AccessTestV2
+{
+    public partial class Form1 : Form
+    {
+        public static string ConnectionString { get; set; } = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source = AccessDbTest.mdb";
+        public static OleDbConnection dbCoonnection;
+        int Id = 0;
+        string Content;
+        public Form1()
+        {
+            InitializeComponent();
+            dbCoonnection = new OleDbConnection(ConnectionString);
+            dbCoonnection.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "accessDbTestDataSet.Person". При необходимости она может быть перемещена или удалена.
+            this.personTableAdapter.Fill(this.accessDbTestDataSet.Person);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            //Content = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            //ContentTb.Text = Content;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Id != 0)
+            {
+                string query = $"DELETE FROM Person WHERE [Код]={Id}";
+                OleDbCommand oleDbCommand = new OleDbCommand(query, dbCoonnection);
+                oleDbCommand.ExecuteNonQuery();
+                MessageBox.Show("Данные удалены");
+                this.personTableAdapter.Fill(this.accessDbTestDataSet.Person);
+            }
+            else
+            {
+                MessageBox.Show("Данные не выбраны");
+            }
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (ContentTb.Text != "")
+            {
+                string query = $"INSERT INTO Person ([Имя]) VALUES ('{ContentTb.Text}')";
+                OleDbCommand oleDbCommand = new OleDbCommand(query, dbCoonnection);
+                // oleDbCommand.ExecuteScalar();
+                oleDbCommand.ExecuteNonQuery();
+                MessageBox.Show("Данные добавлены");
+                this.personTableAdapter.Fill(this.accessDbTestDataSet.Person);
+            }
+            else
+            {
+                MessageBox.Show("Данные не заполнены");
+            }
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            if (Id != 0 && ContentTb.Text != "")
+            {
+                string query = $"UPDATE Person SET Имя = '{ContentTb.Text}' WHERE Код={Id}";
+                OleDbCommand oleDbCommand = new OleDbCommand(query, dbCoonnection);
+                // oleDbCommand.ExecuteScalar();
+                oleDbCommand.ExecuteNonQuery();
+                MessageBox.Show("Данные адаптированы");
+                this.personTableAdapter.Fill(this.accessDbTestDataSet.Person);
+            }
+            else
+            {
+                MessageBox.Show("Данные не выбраны");
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            if (Id > 0)
+            {
+                Content = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                ContentTb.Text = Content;
+            }
+            else
+            {
+                Id = 0;
+                ContentTb.Text = "";
+            }
+        }
+    }
+}
